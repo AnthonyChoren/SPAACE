503425bed7fd67671a5f963edefb5c3187263174
Anthony Choren <tonychoren@gmail.com>
2020-02-17 18:25:27 +0100

    model flat

---
+++ db/migrate/20200217172233_create_flats.rb
@@ -0,0 +1,15 @@
+class CreateFlats < ActiveRecord::Migration[5.2]
+  def change
+    create_table :flats do |t|
+      t.string :name
+      t.string :planet
+      t.integer :capacity
+      t.integer :rating
+      t.text :description
+      t.integer :status
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
