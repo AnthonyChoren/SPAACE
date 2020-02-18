f21da977952b3da043b066b223b39b1e672c5c65
Anthony Choren <tonychoren@gmail.com>
2020-02-18 11:08:24 +0100

    controller booking done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,22 +1,32 @@
 class BookingsController < ApplicationController
   def index
+    @bookings = Booking.all
   end
 
   def show
+    @booking = Booking.find(params[:id])
   end
 
   def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(params[:booking])
+    @booking.save
   end
 
   def edit
+    @booking = Booking.find(params[:id])
   end
 
   def update
+    @booking = Booking.find(params[:id])
+    @booking.update(booking_params)
   end
 
   def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
   end
 end
