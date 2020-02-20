6a2e8d1fe3235bd951fd51ad53266a27b52906db
Sabrina Trachsler <sabrina.trachsler@hotmail.com>
2020-02-20 15:25:50 +0100

    front on bookings#show and bookings#index cards

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -3,7 +3,7 @@ class Flat < ApplicationRecord
   has_many :bookings, dependent: :destroy
 
   validates :name, presence: true, length: { minimum: 3 }
-  validates :planet, presence: true, inclusion: { in: %w(Mars Mercury Venus Earth Jupiter Saturn Uranus Neptune),
+  validates :planet, presence: true, inclusion: { in: %w(Mars Mercury Moon Venus Earth Jupiter Saturn Uranus Neptune),
     message: "%{value} is not a valid planet" }
   validates :capacity, presence: true, numericality: { only_integer: true }
   validates :description, presence: true, length: { minimum: 10 }
